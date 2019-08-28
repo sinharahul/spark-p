7122234f8d6a0f0e01a827a56d172d35a6388f3f
sinharahul <sinharahul@gmail.com>
2019-08-27 22:39:29 -0400

    Adding stratified sampling

--- src/main/scala/explore/CreditCard.scala
+++ src/main/scala/explore/CreditCard.scala
@@ -2,6 +2,8 @@ package explore
 
 import breeze.numerics.round
 import org.apache.spark.ml.feature.{StandardScaler, VectorAssembler}
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 //https://www.kaggle.com/mlg-ulb/creditcardfraud#creditcard.csv
 //https://www.kaggle.com/janiobachmann/credit-fraud-dealing-with-imbalanced-datasets
@@ -9,9 +11,10 @@ import org.apache.spark.sql.SparkSession
   * Detect credit card fraud
   */
 object CreditCard {
+
   def main(args: Array[String]) {
     val creditCard = "/Users/rahulsinha/Downloads/creditcard.csv" // Should be some file on your system
-    val spark = SparkSession.builder.master("local[*]").appName("Simple Application").getOrCreate()
+    val spark = SparkSession.builder.master("local[*]").appName("Credit Card").getOrCreate()
     val logData = spark.read.textFile(creditCard).cache()
     val trainInput = spark.read
                      .option("header", "true")
@@ -36,12 +39,34 @@ object CreditCard {
     println(s"No fraud count ${noFrauds.count()}")
     println(s"fraud count ${frauds.count()}")
     println(s"No frauds are ${nfCnt} % of dataset .Frauds are ${frdCnt}" )
-    val vectorAssember = new VectorAssembler().setInputCols(Array("Amount")).setOutputCol("amount_assem")
-
-    val newDF=vectorAssember.transform(trainInput)
-    val amountDf=new StandardScaler().setInputCol("amount_assem").setOutputCol("scaledAmount")
+    val vectorAssemberAmount = new VectorAssembler().setInputCols(Array("Amount")).setOutputCol("amount_assem")
+    val vectorAssemberTime = new VectorAssembler().setInputCols(Array("Time")).setOutputCol("time_assem")
+    var newDF=vectorAssemberAmount.transform(trainInput)
+    newDF=vectorAssemberTime.transform(newDF)
+    var amountDf=new StandardScaler().setInputCol("amount_assem").setOutputCol("scaledAmount")
     .fit(newDF).transform(newDF)
+    amountDf=new StandardScaler().setInputCol("time_assem").setOutputCol("scaledTime")
+      .fit(amountDf).transform(amountDf)
     amountDf.show(10)
+    amountDf.createOrReplaceTempView("creditcard")
+    spark.sql(
+      """
+        SELECT count(*),Class from creditcard
+         group by Class
+      """.stripMargin).show(10)
+    //0.0017304750013189597
+    val fractions = Map(1 -> 0.0017304750013189597, 0 -> 1.0)
+    val churnDF = amountDf.stat.sampleBy("Class", fractions, 12345L)
+    churnDF.groupBy("Class").count.show()
+    //val arr=MLUtils.kFold(amountDf.toDF().toJavaRDD.rdd,5,0)
+   // amountDf.stat.sampleBy()
+    /**
+    for(a<-arr){
+      val (x,y) = a
+      println(s"x=${x.foreach(println)} y= ${y.foreach(println)}")
+    }
+      */
+    //foldedRDD.foreach(rdd=>rdd.toDF().show(1))
     //  println("No Frauds", round(df['Class'].value_counts()[0]/len(df) * 100,2), '% of the dataset')
     //println('Frauds', round(df['Class'].value_counts()[1]/len(df) * 100,2), '% of the dataset')
    // val amount = new StandardScaler().fit(trainInput.columns.filter(c=>c.equals("Amount"))
@@ -60,4 +85,5 @@ object CreditCard {
   */
     spark.stop()
   }
+
 }
